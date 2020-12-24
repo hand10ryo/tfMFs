96a3a7066d2a13907ecced0df5abd56a0ccc3d12
hand10ryo <hand10ryo@yahoo.co.jp>
2020-12-24 17:39:08 +0900

    add random init option

--- tfMFs/tfCMF.py
+++ tfMFs/tfCMF.py
@@ -5,38 +5,39 @@ from tqdm import tqdm
 
 
 class tfCMF:
-    def __init__(self, alpha=0.7, d_hidden=5, lamda=1, left=lambda x: x ** 2 / 2, right=tf.math.softplus):
+    def __init__(self, alpha=0.7, d_hidden=5, lamda=1,
+                 left=lambda x: x ** 2 / 2, right=tf.math.softplus):
         """ This is a class of CMF to decompose X(n × m) ~ U(n × d) @  V (d × m) and Y(n × l) ~ U(n × d) @ Z(d × l) simultaneously.
         This paper ( http://www.cs.cmu.edu/~ggordon/singh-gordon-kdd-factorization.pdf ) shows detail.
 
         Args:
             alpha (float, optional): hyper-parameter of defining loss rate. Defaults to 0.7.
             d_hidden (int, optional): num of decomposing dimention. Defaults to 5.
-            lamda (int, optional): hyper-parameter of reguralization. Defaults to 1.
-            left (function, optional): link function of R. Defaults to lambdax:x**2/2.
-            right (function, optional): link function of X. Defaults to tf.math.softplus.
+            lamda (float, optional): hyper-parameter of l2 reguralization. Defaults to 1.
+            left (function, optional): link function of X. Defaults to lambdax:x**2/2.
+            right (function, optional): link function of Y. Defaults to tf.math.softplus.
 
-        x^2/2 is link of mse. (lambda x:x**2/2)
-        exp(log(x) + 1) is link of cross entropy. (tf.math.softplus)
-        exp is link of poisson loss. (tf.math.exp)
+        x^2/2 is link function for mse. (lambda x:x**2/2)
+        exp(log(x) + 1) is link function for cross entropy. (tf.math.softplus)
+        exp is link function for poisson loss. (tf.math.exp)
 
         Expmple
         >>> import tfCMF
         >>> import numpy as np
         >>> n,m,d = 1000,2000,5
         >>> X_train = np.random.randint(0,2,[n,m]) # main matrix
-        >>> X_test = X_train
+        >>> X_valid = X_train
         >>> mask_train = np.array([[((i%2 == 0) & (j%2 == 1)) | ((i%2 == 1) & (j%2 == 0)) for i in range(m)]for j in range(n)])
-        >>> mask_test = 1 - mask_train
+        >>> mask_valid = 1 - mask_train
         >>> Y = np.random.random([n,d]) # sideinfo matrix
         >>> cmf = tfCMF.tfCMF(
                 alpha=0.7, d_hidden=5, lamda= 1,
-                left=tfCMF.tf.math.softplus, right=lambda x:x**2/2
+                left=lambda x:x**2/2, right=tfCMF.tf.math.softplus
             )
-        >>> U, V, Z, train_rec, test_rec = cmf.train(
-                X_train, X_test, Y,
-                mask_train = mask_train, mask_test = mask_test,
-                optim_steps=100, verbose=10, early_stopping=5,lr=0.005
+        >>> U, V, Z, train_rec, valid_rec = cmf.train(
+                X_train, X_valid, Y,
+                mask_train = mask_train, mask_valid = mask_valid,
+                optim_steps=100, early_stopping=5,lr=0.005
             )
         """
 
@@ -67,25 +68,36 @@ class tfCMF:
 
         return A, B
 
-    def init_matrices(self, X, Y):
+    def random_init(self, M):
+        A = np.random.rand(M.shape[0], self.d)
+        B = np.random.rand(self.d, M.shape[1])
 
-        U1, V = self.svd_init(X)
-        U2, Z = self.svd_init(Y)
+        return A, B
+
+    def init_matrices(self, X, Y, init="svd"):
+        if init == "svd":
+            U1, V = self.svd_init(X)
+            U2, Z = self.svd_init(Y)
+
+        else:
+            U1, V = self.random_init(X)
+            U2, Z = self.random_init(Y)
+
         U = self.alpha * U1 + (1 - self.alpha) * U2
 
         return U, V, Z
 
-    def train(self, X_train, X_test, Y, mask_train=None, mask_test=None, optim_steps=20000, verbose=100, early_stopping=5, lr=0.005, optimizer="Adam"):
-        """[summary]
+    def train(self, X_train, X_valid, Y, mask_train=None, mask_valid=None,
+              optim_steps=20000, early_stopping=5, lr=0.005, optimizer="Adam", init="svd"):
+        """ Method of training.
 
         Args:
             X_train (matrix): main matrix for train. The shape is (n × m).
-            X_test (matrix): main matrix for test. The shape is (n × m).
+            X_valid (matrix): main matrix for validation. The shape is (n × m).
             Y (matrix): side information matrix. The shape is (n × l)
             mask_train (matrix, optional): mask matrix for train. (If element is False, loss is set to be 0.) The shape is (n × m). Defaults to None.
-            mask_test (matrix, optional): mask matrix for test. The shape is (n × m). Defaults to None.
+            mask_valid (matrix, optional): mask matrix for valid. The shape is (n × m). Defaults to None.
             optim_steps (int, optional): num of epoch. Defaults to 20000.
-            verbose (int, optional): num of step for logs are displayed. Defaults to 100.
             early_stopping (int, optional): num of step for early_stopping. Defaults to 5.
             lr (float, optional): learning rate. Defaults to 0.005.
 
@@ -94,17 +106,17 @@ class tfCMF:
             V.numpy() (ndarray): numpy array of V.
             Z.numpy() (ndarray): numpy array of Z.
             train_loss_record (list) : record of train loss.
-            test_loss_record (list): record of test loss.
+            valid_loss_record (list): record of valid loss.
         """
 
-        U, V, Z = self.init_matrices(X_train, Y)
+        U, V, Z = self.init_matrices(X_train, Y, init=init)
 
         U = tf.keras.backend.variable(U, dtype=tf.float32, name="U")
         V = tf.keras.backend.variable(V, dtype=tf.float32, name="V")
         Z = tf.keras.backend.variable(Z, dtype=tf.float32, name="Z")
 
         X_train = tf.keras.backend.constant(X_train, dtype=tf.float32)
-        X_test = tf.keras.backend.constant(X_test, dtype=tf.float32)
+        X_valid = tf.keras.backend.constant(X_valid, dtype=tf.float32)
         Y = tf.keras.backend.constant(Y, dtype=tf.float32)
 
         if mask_train is None:
@@ -115,13 +127,13 @@ class tfCMF:
         else:
             M_train = tf.keras.backend.constant(mask_train)
 
-        if mask_test is None:
-            M_test = tf.keras.backend.constant(
-                np.ones(X_test.shape).astype(int))
-        elif mask_test == "nonzero":
-            M_test = tf.keras.backend.constant((X_test != 0).astype(int))
+        if mask_valid is None:
+            M_valid = tf.keras.backend.constant(
+                np.ones(X_valid.shape).astype(int))
+        elif mask_valid == "nonzero":
+            M_valid = tf.keras.backend.constant((X_valid != 0).astype(int))
         else:
-            M_test = tf.keras.backend.constant(mask_test)
+            M_valid = tf.keras.backend.constant(mask_valid)
 
         def loss(X=X_train, Y=Y, M=M_train, alpha=self.alpha, lamda=self.lamda):
             X_ = M * tf.matmul(U, V)
@@ -146,28 +158,24 @@ class tfCMF:
         stop_time = 0
         times, old_loss = 0, 100000
         train_loss_record = []
-        test_loss_record = []
+        valid_loss_record = []
 
         for times in tqdm(range(optim_steps)):
             loss_train = loss(X=X_train, Y=Y, M=M_train).numpy()
-            loss_test = loss(X=X_test, Y=Y, M=M_test).numpy()
+            loss_valid = loss(X=X_valid, Y=Y, M=M_valid).numpy()
             train_loss_record.append(loss_train)
-            test_loss_record.append(loss_test)
+            valid_loss_record.append(loss_valid)
 
-            if loss_test > old_loss:
+            if loss_valid > old_loss:
                 if stop_time == early_stopping:
-                    # print("[Info] At last time-step {}, valid data loss is {}".format(times, loss_test))
                     break
                 else:
                     stop_time += 1
             else:
                 stop_time = 0
 
-            old_loss = loss_test
-            # if verbose > 0:
-            # if times % verbose == 0:
-            # print("[Info] At time-step {}, valid data loss is {}".format(times, loss_test))
+            old_loss = loss_valid
 
             opt.minimize(loss=loss, var_list=[U, V, Z])
 
-        return U.numpy(), V.numpy(), Z.numpy(), train_loss_record, test_loss_record
+        return U.numpy(), V.numpy(), Z.numpy(), train_loss_record, valid_loss_record
