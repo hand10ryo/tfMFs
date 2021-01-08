1e47558d413ea4760e0ff3eeeca42e13efd260d4
hand10ryo <hand10ryo@yahoo.co.jp>
2021-01-08 18:05:49 +0900

    add at 20120108

--- experiments/dataset.py
+++ experiments/dataset.py
@@ -1,5 +1,3 @@
-
-
 import random
 import string
 import numpy as np
@@ -18,23 +16,37 @@ def make_random_words_count(numusers, lam=0.3):
 
 
 def create_synthetic_data(num_pos_users=200, num_neg_users=200, num_random_words=1000,
-                          alpha=0.7, lam=0.001, shape=0.1, scale=2):
+                          alpha=0.7, lam=0.001, shape=0.1, scale=2,
+                          pos_lam=0, neg_lam=0, pos_shape=0, neg_shape=0, pos_scale=0, neg_scale=0):
 
     pos_labels = [1] * num_pos_users
     neg_labels = [0] * num_neg_users
 
     # create synthetic corpus
     if lam is None:
-        pos_corpus = [make_random_words_count(num_pos_users, lam=np.random.gamma(
-            shape=shape, scale=scale)) for i in range(num_random_words)]
-        neg_corpus = [make_random_words_count(num_neg_users, lam=np.random.gamma(
-            shape=shape, scale=scale)) for i in range(num_random_words)]
+        pos_corpus = [
+            make_random_words_count(
+                num_pos_users,
+                lam=max(1e-5, pos_lam +
+                        np.random.gamma(shape=shape + pos_shape, scale=scale + pos_scale))
+            )
+            for i in range(num_random_words)
+        ]
+
+        neg_corpus = [
+            make_random_words_count(
+                num_neg_users,
+                lam=max(1e-5, neg_lam +
+                        np.random.gamma(shape=shape + neg_shape, scale=scale + neg_scale))
+            )
+            for i in range(num_random_words)
+        ]
 
     else:
         pos_corpus = [make_random_words_count(
-            num_pos_users, lam=lam) for i in range(num_random_words)]
+            num_pos_users, lam=max(1e-5, lam + pos_lam)) for i in range(num_random_words)]
         neg_corpus = [make_random_words_count(
-            num_neg_users, lam=lam) for i in range(num_random_words)]
+            num_neg_users, lam=max(1e-5, lam + neg_lam)) for i in range(num_random_words)]
 
     # convart to csr_matrix
     X = vstack([csr_matrix(pos_corpus).T, csr_matrix(neg_corpus).T])
